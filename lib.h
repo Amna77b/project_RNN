e3a3e14204f2d3771461f474b6a37be0030caacb
Emnaaa <138696848+Amna77b@users.noreply.github.com>
2024-05-10 15:09:46 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,9 +1,9 @@
-#ifndef LIB_H_
-#define LIB_H_
+// cette bibiliothéque contient les fonctions d'activation 
 
 #include <stdio.h>
 #include <math.h>
-
+#ifndef LIB_H_
+#define LIB_H_
 
 // Fonction d'activation ReLU
 float relu(float x) {
@@ -11,14 +11,27 @@ float relu(float x) {
 }
 // Fonction d'activation Softmax
 void softmax(float *inputs, int num_inputs) {
-    float sum = 0.0;
-    for (int i = 0; i < num_inputs; i++) {
-        sum += expf(inputs[i]);
+    double max_xi = x[0];
+    double sum_exp = 0.0;
+
+    //Rechercher le maximum dans le tableau d'entrées
+    for (int i = 1; i < numb_xi; i++) {
+        if (x[i] > max_xi) {
+            max_xi = x[i];
+        }
     }
-    for (int i = 0; i < num_inputs; i++) {
-        inputs[i] = expf(inputs[i]) / sum;
+
+    //Calculer la somme exponentielle
+    for (int i = 0; i < numb_xi; i++) {
+        sum_exp += exp(x[i] - max_xi); // Soustraction du maximum pour éviter le débordement numérique
+    }
+
+    
+    for (int i = 0; i < numb_xi; i++) {
+        yi[i] = exp(x[i] - max_xi) / sum_exp;
     }
 }
+
 // Fonction Sigmoid
 float sigmoid(float x)
 {
